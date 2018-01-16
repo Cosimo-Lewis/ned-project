534e9eb11ade1a926539c68aeef7e6da4e52e8a2
Cosimo-Lewis <lewis.c@etoncollege.org.uk>
2018-01-16 18:56:41 +0000

    first upload

---
+++ medicalApp copy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  medicalApp
+//
+//  Created by Cosimo Lewis on 16/01/2018.
+//  Copyright © 2018 Cosimo Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
